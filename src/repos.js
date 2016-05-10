d6d3d2c68fbf49365967e1b696e60e90c66e37c9
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2016-05-10 19:15:40 +0530

    Add in all Ember repos to be tracked
    Closes #9

--- src/repos.js
+++ src/repos.js
@@ -1,21 +1,21 @@
-export default {
-  'sivakumar-kailasam/github-webook-test-repo': {
-    labels: [
-      'help wanted',
-      'a11y',
-      'bug',
-      'question',
-    ]
-  },
-  'acorncom/ember-hitlist-tester': {
-    labels: [
-      'Help Wanted'
-    ]
-  },
-  'emberjs/ember.js': {
-    labels: [
-      'Needs Help',
-      'Good for New Contributors'
-    ]
-  }
-};
+const standardLabels = [
+  'Bug',
+  'Help Wanted'
+];
+
+let repoList = [
+  'ember-learn/ember-help-wanted',
+  'ember-learn/ember-help-wanted-webhook',
+  'emberjs/guides',
+  'emberjs/website',
+  'emberjs/ember.js',
+  'ember-cli/ember-cli',
+];
+
+let repoHash = {};
+
+repoList.map(repo => {
+  repoHash[repo] = { labels: standardLabels };
+});
+
+export default repoHash;
